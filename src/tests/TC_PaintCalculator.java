0cffe20cff69f97ef9dc72e0e0ea912d41b0e547
Andrew Parra <andrew.parra@wellspring.com>
2018-12-11 17:11:22 -0600

    Initializing Driver Closing Driver Going to home page

--- src/tests/TC_PaintCalculator.java
+++ src/tests/TC_PaintCalculator.java
@@ -1,14 +1,48 @@
 package tests;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import pages.Page_Dimensions;
+import pages.Page_Home;
+import pages.Page_Results;
+
 public class TC_PaintCalculator {
+    private WebDriver driver;
+    private Page_Home homePage;
+    private Page_Dimensions dimensionsPage;
+    private Page_Results resultsPage;
+
+    @BeforeTest
+    public void setup_InitializeDriver() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+    
+    @AfterTest
+    public void teardown_CloseDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+    
+    @BeforeMethod
+    public void setup_GoToHomePage() {
+        homePage = new Page_Home(driver);
+        homePage.visitPage();
+    }
     
     @Test(dataProvider = "Calculate Results for a Room")
     public void test_CalculateResultsForRoom(int length, int with, int height, 
             int expectedWallarea, double expectedGallonsOfPaint) {
-        // TODO Create Test
     }
     
     @DataProvider(name = "Calculate Results for a Room")
@@ -23,6 +57,5 @@ public class TC_PaintCalculator {
         
         return testData;
     }
-    
     
 }
