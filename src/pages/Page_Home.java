463a47de2661c76305cbef7d3e80e8a9c4cc37fb
Andrew Parra <andrew.parra@wellspring.com>
2018-12-12 14:00:12 -0600

    Finished TODO's on Home Page

--- src/pages/Page_Home.java
+++ src/pages/Page_Home.java
@@ -4,7 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class Page_Home extends Page_Base {
-
+    
+    private static final By NUMBER_OF_ROOMS_FORM = By.cssSelector("[action='dimensions']");
+    private static final By ROOMS_INPUT = By.name("rooms");
+    private static final By SUBMIT_BUTTON = By.cssSelector("[type='submit']");
+    
     public Page_Home(WebDriver driver) {
         super(driver);
     }
@@ -14,13 +18,17 @@ public class Page_Home extends Page_Base {
         return this;
     }
 
-    public Page_Home enterNumberOfRooms(int i) {
-        // TODO Auto-generated method stub
+    public Page_Home enterNumberOfRooms(int numberOfRooms) {
+        driver.findElement(NUMBER_OF_ROOMS_FORM)
+                .findElement(ROOMS_INPUT)
+                .sendKeys(Integer.toString(numberOfRooms));
         return this;
     }
 
     public Page_Dimensions submitRoomsForm() {
-        // TODO Auto-generated method stub
+        driver.findElement(NUMBER_OF_ROOMS_FORM)
+                .findElement(SUBMIT_BUTTON)
+                .click();
         return new Page_Dimensions(driver);
     }
 
